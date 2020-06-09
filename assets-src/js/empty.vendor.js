836e7e25b1f2989eedeca19eaeb74db34a23340a
Edgar Tec <edgartec87@gmail.com>
2020-06-09 16:57:28 -0500

    Se modificó la estructura de los recursos de la carpeta assets para adaptarlo a Gulp

--- assets-src/js/empty.vendor.js
+++
@@ -1,3 +0,0 @@
-// @codekit-append  "../vendor/jquery/jquery.min.js";
-// @codekit-append  "../vendor/bootstrap/bootstrap.bundle.min";
-// @codekit-append  "./_super-fresco.js";
--- assets-src/js/product.vendor.js
+++
@@ -1,6 +0,0 @@
-// @codekit-append  "../vendor/jquery/jquery.min.js";
-// @codekit-append  "../vendor/bootstrap/bootstrap.bundle.min";
-// @codekit-append  "../vendor/cleave/cleave.min.js";
-// @codekit-append  "../vendor/jquery-validation/jquery.validate.min.js";
-// @codekit-append  "../vendor/jquery-input-filter/jquery-input-filter.js";
-// @codekit-append  "./_super-fresco.js";
