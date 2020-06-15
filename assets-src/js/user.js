243fbe2de530db9edb2d74b4f210d389db04c35d
Edgar Tec <edgartec87@gmail.com>
2020-06-15 14:37:39 -0500

    Se modificó el script de editar y nuevo usuario para implementar el avatar de usuario

--- assets-src/js/user.js
+++ assets-src/js/user.js
@@ -1,8 +1,10 @@
 var slugRegex = /^[a-z0-9\-\_]*$/i;
 $(document).ready(function () {
+
 	$.validator.addMethod("slugRegex", function (value, element) {
 		return this.optional(element) || slugRegex.test(value);
 	}, "Username must contain only letters, numbers, underscores or dashes.");
+
 	$("#userForm").validate({
 		onfocusout: function (element) {
 			this.element(element);
@@ -51,4 +53,63 @@ $(document).ready(function () {
 			$(element).removeClass("is-invalid");
 		}
 	});
+
+	var $uploadCrop;
+	var $fileInput = $('#fileInput');
+
+	function fileInputChange() {
+		$("#modalCrop").modal("show");
+		$uploadCrop = $('#croppie').croppie({
+			viewport: {
+				width: 250,
+				height: 250,
+				type: 'circle'
+			},
+			boundary: {
+				width: 250,
+				height: 250
+			},
+			enableOrientation: true
+		});
+		if (this.files && this.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				$uploadCrop.croppie('bind', {
+					url: e.target.result
+				}).then(function () {
+					console.log('jQuery bind complete');
+				});
+			}
+			reader.readAsDataURL(this.files[0]);
+		}
+		console.log("se resetea");
+		$(this).val('');
+	}
+
+	$fileInput.change(fileInputChange);
+	$("#modalCrop").on("hidden.bs.modal", function () {
+		$uploadCrop.croppie('destroy');
+	});
+
+	$("#setAvatar").click(function (e) {
+		e.preventDefault();
+		$uploadCrop.croppie('result', 'base64')
+			.then((base64) => {
+				$('#removeAvatar').prop('disabled', false);
+				$('#imagePreview').attr('src', base64);
+				$('#avatar').val(base64);
+				$('#nullAvatar').val(0);
+				$("#modalCrop").modal("hide");
+			});
+	})
+
+	$('#removeAvatar').click(function (e) {
+		e.preventDefault();
+		let defaultSrc = $('#imagePreview').data('src');
+		$('#imagePreview').attr('src', defaultSrc);
+		$('#avatar').val('');
+		$('#nullAvatar').val(1);
+		$('#removeAvatar').prop('disabled', true);
+	});
+
 });
